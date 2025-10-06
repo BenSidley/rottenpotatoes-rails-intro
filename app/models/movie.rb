5d63c41da8d24c0d427f187b74fcdab98d2ac0d0
Benjamin SIdley <bms2227@columbia.edu>
2025-10-06 18:10:13 +0000

    part1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
-class Movie < ActiveRecord::Base
+class Movie < ApplicationRecord
+  def self.all_ratings
+    distinct.order(:rating).pluck(:rating)
+  end
+
+  def self.with_ratings(ratings_list)
+    return all if ratings_list.blank?
+    where(rating: ratings_list.map { |r| r.to_s.upcase })
+  end
 end
